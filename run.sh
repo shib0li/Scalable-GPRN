fecc1234d6d54ff8ae7a63f9324561fda6e121c1
Shibo Li <shiboli.cs@gmail.com>
2022-12-20 18:48:32 +0000

    init

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python main.py --domain jura \
-               --kernel ard \
-               --device 0 \
-               --rank 2 \
-               --maxIter 20000 \
-               --interval 200 
-
